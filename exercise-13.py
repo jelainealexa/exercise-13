cbec501de79af62cc20610abcb5484fe0c45f7b4
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-23 18:23:50 +0800

    Final

--- exercise-13.py
+++ exercise-13.py
@@ -1,10 +1,10 @@
 # Print multiplication table from 1 to 10
 
 # Outer loop
-for i in range(1, 10):
+for i in range(1, 11):
     # Inner loop
-    for j in range(1, 10):
+    for j in range(1, 11):
         # Print
-        print(i*j, end="")
+        print(i*j, end=" ")
 # Print
     print()
